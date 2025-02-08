78092fd344cb6eb60f60679038fe95d98e61a075
marshmar <goekazld123@naver.com>
2025-02-09 00:30:47 +0900

    사운드 매니저 업데이트

--- PageFinder/Assets/02.Scripts/Mananger/AudioManager.cs
+++ PageFinder/Assets/02.Scripts/Mananger/AudioManager.cs
@@ -1,31 +1,126 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
-public static class AudioClips
+public enum SoundType
 {
-    public const string dashClipName = "dashFX";
+    Bgm,
+    Effect,
+    MaxCount // Sound Enum의 개수 체크
 }
 
-[RequireComponent(typeof(AudioSource))]
+public static class SoundPath
+{
+    #region Player
+    // BasicAttack
+    public const string attack1VfxPath = "Sounds/sfx_basic_attack_01";
+    public const string attack2VfxPath = "Sounds/sfx_basic_attack_02";
+    public const string attack3VfxPath = "Sounds/sfx_basic_attack_03";
+
+    // Dash
+    public const string dashVfx1Path = "Sounds/sfx_ink_dash_01";
+    #endregion
+
+    #region BGM
+    public const string bgmPath = "Sounds/BattleBGM";
+    #endregion
+
+}
 public class AudioManager : Singleton<AudioManager>
 {
-    private AudioSource audioSource;
-    public Dictionary<string, AudioClip> audioClips = new Dictionary<string, AudioClip>();
+    private AudioSource[] audioSources = new AudioSource[(int)SoundType.MaxCount];
+    private Dictionary<string, AudioClip> audioClips = new Dictionary<string, AudioClip>();
 
     private void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        Init();
+        Play(SoundPath.bgmPath, SoundType.Bgm);
     }
 
-
-    public void PlayAudioOneShot(string clipName)
+    private void OnDestroy()
     {
-        audioSource.PlayOneShot(audioClips[clipName]);
+        Clear();
     }
 
-    public void PlayAudioOneShot(string clipName, float duration)
+    private void Init()
     {
-        audioSource.PlayOneShot(audioClips[clipName]);
+        string[] soundTypes = System.Enum.GetNames(typeof(SoundType));
+        for(int i = 0; i < soundTypes.Length -1; i++)
+        {
+            GameObject soundPlayer = new GameObject { name = soundTypes[i] };
+            audioSources[i] = soundPlayer.AddComponent<AudioSource>();
+            soundPlayer.transform.parent = this.transform;
+        }
+
+        audioSources[(int)SoundType.Bgm].loop = true; // bgm 재생기는 반복 재생
+    }
+
+    public void Clear()
+    {
+        foreach(AudioSource audioSource in audioSources)
+        {
+            audioSource.clip = null;
+            audioSource.Stop();
+        }
+    }
+
+    public void Play(AudioClip audioClip, SoundType type = SoundType.Effect, float pitch = 1.0f)
+    {
+        if (audioClip is null) return;
+
+        if(type == SoundType.Bgm)
+        {
+            AudioSource audioSource = audioSources[(int)SoundType.Bgm];
+            if (audioSource.isPlaying)
+                audioSource.Stop();
+
+            audioSource.pitch = pitch;
+            audioSource.clip = audioClip;
+            audioSource.Play();
+            Debug.Log("BGM 재생");
+        }
+        else if(type == SoundType.Effect)
+        {
+            AudioSource audioSource = audioSources[(int)SoundType.Effect];
+            audioSource.pitch = pitch;
+            audioSource.PlayOneShot(audioClip);
+        }
+    }
+
+    public void Play(string path, SoundType type = SoundType.Effect, float pitch = 1.0f)
+    {
+        AudioClip audioClip = GetOrAddAudioClip(path, type);
+        Play(audioClip, type, pitch);
+    }
+
+    AudioClip GetOrAddAudioClip(string path, SoundType type = SoundType.Effect)
+    {
+        if (path.Contains("Sounds/") == false)
+            path = $"Sounds/{path}"; // Sound 폴더 안에 없으면 Sound 폴더안에 저장
+
+        AudioClip audioClip = null;
+
+        if(type == SoundType.Bgm)
+        {
+            if(audioClips.TryGetValue(path, out audioClip) == false)
+            {
+                audioClip = Resources.Load<AudioClip>(path);
+                audioClips.Add(path, audioClip);
+            }
+        }
+        else
+        {
+            if(audioClips.TryGetValue(path, out audioClip) == false)
+            {
+                audioClip = Resources.Load<AudioClip>(path);
+                audioClips.Add(path, audioClip);
+            }
+        }
+
+        if (audioClip == null)
+            Debug.LogError($"AudioClip Missing ! {path}");
+
+        return audioClip;
     }
 }
