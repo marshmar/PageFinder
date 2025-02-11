78c64cbdb85bb94dcb583b74cb3c3a1d0fe6f083
Jerry <50892930+lsjsos@users.noreply.github.com>
2025-02-11 16:29:04 +0900

    절차적 맵 생성 2차 코드 리팩토링
    - Generate NodeManager.cs for managing Node datas.
    - Node data의 순환 참조 방지
    - 알고리즘 계산 관련 함수를 Utils 전역 class로 분리

---
+++ PageFinder/Assets/02.Scripts/Map/Data/Node.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum NodeType { Unknown, Start, Battle_Normal, Battle_Elite, Quest, Treasure, Market, Comma, Boss }
+
+[Serializable]
+public class Node
+{
+    public int id { get; private set; }
+    private static int nextID = 0;
+
+    public int column;
+    public int row;
+    public Vector2 position;
+    public NodeType type;
+    public List<int> neighborIDs = new();
+    public Node prevNode;
+    public GameObject map;
+
+    public Node(int column, int row, Vector2 position, NodeType type, GameObject map)
+    {
+        id = nextID++;
+        this.column = column;
+        this.row = row;
+        this.position = position;
+        this.type = type;
+        this.map = map;
+    }
+}
