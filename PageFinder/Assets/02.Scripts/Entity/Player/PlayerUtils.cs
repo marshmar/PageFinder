f6523e7277fa9804851010c5dcef6b02cc341982
haedam <goekazld123@naver.com>
2025-02-25 22:35:57 +0900

    플레이어 기본 이동 NewInputSystme으로 변경
    - 플레이어 기본 이동 버튼(W,A,S,D) New Input System으로 변경
    - 대쉬 변경 진행중

--- PageFinder/Assets/02.Scripts/Entity/Player/PlayerUtils.cs
+++ PageFinder/Assets/02.Scripts/Entity/Player/PlayerUtils.cs
@@ -13,6 +13,7 @@ public class PlayerUtils : MonoBehaviour
     public Transform Tr { get => tr; set => tr = value; }
     public Rigidbody Rigid { get => rigid; set => rigid = value; }
 
+    public float rotateSpeed = 10f;
     // Start is called before the first frame update
     void Awake()
     {
@@ -21,10 +22,16 @@ public class PlayerUtils : MonoBehaviour
         rigid = DebugUtils.GetComponentWithErrorLogging<Rigidbody>(this.gameObject, "Rigidbody");
     }
 
-    public void TurnToDirection(Vector3 dir)
+    public void TurnToDirection(Vector3 dir, bool smoothRot = false)
     {
         if (dir == Vector3.zero) return;
-        modelTr.rotation = Quaternion.LookRotation(new Vector3(dir.x, 0f, dir.z));
+
+        if (smoothRot)
+        {
+            Quaternion newRot = Quaternion.LookRotation(new Vector3(dir.x, 0f, dir.z));
+            modelTr.rotation = Quaternion.Slerp(modelTr.rotation, newRot, rotateSpeed * Time.deltaTime);
+        }
+        else modelTr.rotation = Quaternion.LookRotation(new Vector3(dir.x, 0f, dir.z));
     }
 
     public Vector3 CalculateDirectionFromPlayer(Component component)
