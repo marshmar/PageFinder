0fee5da5d39a2f3fb900de56d6176751879c0150
Jerry <50892930+lsjsos@users.noreply.github.com>
2025-04-05 00:19:14 +0900

    잉크 마크 합성 기능 구현
    - RenderTexture 캡쳐 방식 잉크 마크 합성 기능 구현 및 테스트 완료

--- PageFinder/Assets/02.Scripts/Object/InkMarkSynthesis.cs
+++ PageFinder/Assets/02.Scripts/Object/InkMarkSynthesis.cs
@@ -4,148 +4,119 @@ public class InkMarkSynthesis : Singleton<InkMarkSynthesis>
 {
     public GameObject prefab;
 
-    /// <summary>
-    /// Synthesize two InkMark Objects
-    /// </summary>
-    /// <param name="a">First InkMark</param>
-    /// <param name="b">Second InkMark</param>
-    /// <param name="pixelPerUnit">Pixel Per Unit</param>
     public void Synthesize(GameObject a, GameObject b)
     {
         var inkMarkA = a.GetComponent<InkMark>();
         var inkMarkB = b.GetComponent<InkMark>();
 
         var newInkType = InkMarkSetter.Instance.SetMergedInkType(inkMarkA.CurrType, inkMarkB.CurrType);
-        var newSprite = InkMarkSetter.Instance.SetSprite(newInkType);
+        var seamlessSprite = InkMarkSetter.Instance.SetSprite(newInkType);
+        var seamlessTexture = seamlessSprite.texture;
 
-        SpriteMask maskA = a.GetComponentInChildren<SpriteMask>();
-        SpriteMask maskB = b.GetComponentInChildren<SpriteMask>();
+        inkMarkA.FadeOut = true;
+        inkMarkB.FadeOut = true;
 
-        Sprite spriteA = maskA.sprite;
-        Sprite spriteB = maskB.sprite;
+        // 1. Set both ink prefabs and all child objects to the INKMARK layer.
+        int inkMarkLayer = LayerMask.NameToLayer("INKMARK");
+        SetLayerRecursively(a, inkMarkLayer);
+        SetLayerRecursively(b, inkMarkLayer);
 
-        // 월드 상의 SpriteMask 위치 (중심)
-        Vector3 posA = maskA.transform.position;
-        Vector3 posB = maskB.transform.position;
+        // 2. Calculating the center positions of a and b (based on SpriteRenderer)
+        SpriteRenderer aSR = a.GetComponentInChildren<SpriteRenderer>();
+        SpriteRenderer bSR = b.GetComponentInChildren<SpriteRenderer>();
 
-        // 각 스프라이트의 픽셀 크기를 pixelPerUnit을 이용해 월드 크기로 변환
-        float pixelPerUnit = 200;
-        float widthWorldA = spriteA.rect.width / pixelPerUnit;
-        float heightWorldA = spriteA.rect.height / pixelPerUnit;
-        float widthWorldB = spriteB.rect.width / pixelPerUnit;
-        float heightWorldB = spriteB.rect.height / pixelPerUnit;
+        Vector3 centerA = aSR.bounds.center;
+        Vector3 centerB = bSR.bounds.center;
+        Vector3 inkCenter = (centerA + centerB) / 2f;
 
-        // 두 스프라이트의 월드 바운딩 박스 계산 (겹침 여부와 상관없이 실제 위치 반영)
-        float left = Mathf.Min(posA.x - widthWorldA * 0.5f, posB.x - widthWorldB * 0.5f);
-        float right = Mathf.Max(posA.x + widthWorldA * 0.5f, posB.x + widthWorldB * 0.5f);
-        float bottom = Mathf.Min(posA.y - heightWorldA * 0.5f, posB.y - heightWorldB * 0.5f);
-        float top = Mathf.Max(posA.y + heightWorldA * 0.5f, posB.y + heightWorldB * 0.5f);
+        // 3. Create a temporary camera and take a picture from above
+        GameObject tempCameraObj = new("InkTempCamera");
+        Camera tempCamera = tempCameraObj.AddComponent<Camera>();
+        tempCamera.clearFlags = CameraClearFlags.SolidColor;
+        tempCamera.backgroundColor = new Color(0, 0, 0, 0);
+        tempCamera.cullingMask = LayerMask.GetMask("INKMARK");
+        tempCamera.transform.position = inkCenter + new Vector3(0, 10f, 0);
+        tempCamera.transform.rotation = Quaternion.Euler(90f, 180f, 0f);
 
-        // 최종 합성 텍스처의 크기 (픽셀 단위)
-        int compositePixelWidth = Mathf.RoundToInt((right - left) * pixelPerUnit);
-        int compositePixelHeight = Mathf.RoundToInt((top - bottom) * pixelPerUnit);
+        // 4. Create a RenderTexture (adjust resolution to suit your needs)
+        int width = 1024;
+        int height = 1024;
+        RenderTexture rt = new(width, height, 24);
+        tempCamera.targetTexture = rt;
+        tempCamera.Render();
 
-        Texture2D newTexture = new Texture2D(compositePixelWidth, compositePixelHeight, TextureFormat.RGBA32, false);
-        Color[] combinedPixels = new Color[compositePixelWidth * compositePixelHeight];
+        // 5. Reading Texture2D from RenderTexture
+        RenderTexture.active = rt;
+        Texture2D capturedTexture = new(width, height, TextureFormat.ARGB32, false);
+        capturedTexture.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+        capturedTexture.Apply();
 
-        // 각 스프라이트의 월드 좌측하단 좌표 계산
-        float spriteAWorldLowerLeftX = posA.x - (widthWorldA * 0.5f);
-        float spriteAWorldLowerLeftY = posA.y - (heightWorldA * 0.5f);
-        float spriteBWorldLowerLeftX = posB.x - (widthWorldB * 0.5f);
-        float spriteBWorldLowerLeftY = posB.y - (heightWorldB * 0.5f);
+        // 6. Initialize RenderTexture
+        tempCamera.targetTexture = null;
+        RenderTexture.active = null;
+        rt.Release();
+        Destroy(tempCameraObj);
 
-        // 합성 텍스처 내에서 각 스프라이트의 픽셀 오프셋 (월드 좌표와 pixelPerUnit을 이용)
-        int offsetXA = Mathf.RoundToInt((spriteAWorldLowerLeftX - left) * pixelPerUnit);
-        int offsetYA = Mathf.RoundToInt((spriteAWorldLowerLeftY - bottom) * pixelPerUnit);
-        int offsetXB = Mathf.RoundToInt((spriteBWorldLowerLeftX - left) * pixelPerUnit);
-        int offsetYB = Mathf.RoundToInt((spriteBWorldLowerLeftY - bottom) * pixelPerUnit);
-
-        // 각 스프라이트의 픽셀 데이터를 합성 텍스처의 올바른 위치에 복사합니다.
-        CopyMaskedPixels(spriteA, combinedPixels, compositePixelWidth, offsetXA, offsetYA);
-        CopyMaskedPixels(spriteB, combinedPixels, compositePixelWidth, offsetXB, offsetYB);
-
-        // seamless sprite의 텍스처 데이터를 이용해 색상을 입힙니다.
-        ApplySeamlessTexture(combinedPixels, newSprite, compositePixelWidth, compositePixelHeight);
-
-        newTexture.SetPixels(combinedPixels);
-        newTexture.Apply();
-
-        // 합성된 결과를 담을 오브젝트 생성
-        GameObject newObject = Instantiate(prefab);
-
-        // 바운딩 박스의 중심으로 위치 설정
-        newObject.transform.position = new Vector3((left + right) * 0.5f, (bottom + top) * 0.5f, (posA.z + posB.z) * 0.5f);
-        newObject.GetComponent<InkMark>().CurrType = newInkType;
-
-        SpriteRenderer srNew = newObject.GetComponentInChildren<SpriteRenderer>();
-        SpriteMask newSpriteMask = newObject.GetComponentInChildren<SpriteMask>();
-
-        if (newSpriteMask != null)
+        // 7. Color mapping of seamlessTexture to the part of the captured texture where the alpha value is greater than 0
+        Texture2D processedTexture = new(width, height, TextureFormat.ARGB32, false);
+        for (int y = 0; y < height; y++)
         {
-            newSpriteMask.sprite = Sprite.Create(newTexture, new Rect(0, 0, compositePixelWidth, compositePixelHeight), new Vector2(0.5f, 0.5f), pixelPerUnit);
+            for (int x = 0; x < width; x++)
+            {
+                Color capturedPixel = capturedTexture.GetPixel(x, y);
+                if (capturedPixel.a > 0)
+                {
+                    // Convert x, y to UV coordinates in the range 0 to 1(seamlessTexture assumes tileable)
+                    float u = (float)x / width;
+                    float v = (float)y / height;
+                    Color seamlessColor = seamlessTexture.GetPixelBilinear(u, v);
+                    processedTexture.SetPixel(x, y, seamlessColor);
+                }
+                else
+                {
+                    processedTexture.SetPixel(x, y, capturedPixel);
+                }
+            }
         }
-        srNew.sprite = newSpriteMask.sprite;
+        processedTexture.Apply();
 
-        // Set Unique Sorting Order (inkId based)
+        // 8. Create Sprite from processedTexture
+        Sprite newSprite = Sprite.Create(processedTexture, new Rect(0, 0, width, height), new Vector2(0.5f, 0.5f));
+
+        // 9. Create a composite ink prefab by instantiating the prefab.
+        GameObject synthesizedInk = Instantiate(prefab, inkCenter, Quaternion.Euler(90, 0, 0));
+
+        // 10. Assign a new Sprite to a Sprite Renderer
+        SpriteRenderer sr = synthesizedInk.GetComponentInChildren<SpriteRenderer>();
+        sr.sprite = newSprite;
+
+        // 11. Assign a new Sprite to a Sprite Mask
+        SpriteMask sm = synthesizedInk.GetComponentInChildren<SpriteMask>();
+        sm.sprite = newSprite;
+
+        // 12. Set Unique Sorting Order (inkId based)
         int baseOrder = InkMarkSetter.inkId * 10;
         InkMarkSetter.inkId++;
-        srNew.sortingOrder = baseOrder + 1;
+        sr.sortingOrder = baseOrder + 1;
+        sm.frontSortingOrder = baseOrder + 1;
+        sm.backSortingOrder = baseOrder;
+        sm.isCustomRangeActive = true;
 
-        newSpriteMask.frontSortingOrder = baseOrder + 1;
-        newSpriteMask.backSortingOrder = baseOrder;
-        newSpriteMask.isCustomRangeActive = true;
+        Debug.Log("Synthesized");
 
-        Destroy(a);
-        Destroy(b);
+        InkMark ink = synthesizedInk.GetComponent<InkMark>();
+        ink.SetSynthesizedInkMarkData(InkMarkType.SYNTHESIZED);
     }
 
-    // 주어진 Sprite의 픽셀 데이터를 targetPixels에 복사합니다.
-    // targetPixels는 합성 텍스처의 픽셀 배열이고, offsetX/Y는 복사 시작 위치(픽셀 단위)입니다.
-    void CopyMaskedPixels(Sprite sprite, Color[] targetPixels, int targetWidth, int offsetX, int offsetY)
+    // A function that recursively sets the layers of a GameObject and all of its children.
+    private void SetLayerRecursively(GameObject obj, int layer)
     {
-        Rect rect = sprite.textureRect;
-        int width = (int)rect.width;
-        int height = (int)rect.height;
-        Color[] spritePixels = sprite.texture.GetPixels((int)rect.x, (int)rect.y, width, height);
+        if (obj == null) return;
 
-        for (int y = 0; y < height; y++)
+        obj.layer = layer;
+        foreach (Transform child in obj.transform)
         {
-            for (int x = 0; x < width; x++)
-            {
-                int targetIndex = (y + offsetY) * targetWidth + (x + offsetX);
-                int spriteIndex = y * width + x;
-                if (spritePixels[spriteIndex].a > 0)
-                {
-                    targetPixels[targetIndex] = spritePixels[spriteIndex];
-                }
-            }
-        }
-    }
-
-    // newSprite의 텍스처 데이터를 샘플링하여, 합성 텍스처의 불투명 영역에 색상을 적용합니다.
-    void ApplySeamlessTexture(Color[] pixels, Sprite sprite, int width, int height)
-    {
-        Texture2D texture = sprite.texture;
-        Rect spriteRect = sprite.rect;
-        int textureWidth = (int)spriteRect.width;
-        int textureHeight = (int)spriteRect.height;
-        Color[] texturePixels = texture.GetPixels((int)spriteRect.x, (int)spriteRect.y, textureWidth, textureHeight);
-
-        for (int y = 0; y < height; y++)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                int index = y * width + x;
-                float u = (float)x / width;
-                float v = (float)y / height;
-                int tx = Mathf.FloorToInt(u * textureWidth) % textureWidth;
-                int ty = Mathf.FloorToInt(v * textureHeight) % textureHeight;
-                int textureIndex = ty * textureWidth + tx;
-                if (pixels[index].a > 0)
-                {
-                    pixels[index] = texturePixels[textureIndex];
-                }
-            }
+            SetLayerRecursively(child.gameObject, layer);
         }
     }
 }
